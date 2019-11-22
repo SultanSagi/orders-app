94823044c844153870376a85e7d1566bb0e02af8
Sultan <sulasagi@gmail.com>
2019-11-22 13:13:41 +0600

    Reduce form duplication

---
+++ resources/views/orders/_form.blade.php
@@ -0,0 +1,35 @@
+<div class="form-group">
+    <label for="user_id" class="col-form-label">User</label>
+    <select id="user_id" class="form-control{{ $errors->has('user_id') ? ' is-invalid' : '' }}" name="user_id">
+        @foreach ($users as $user => $label)
+            <option value="{{ $user }}"{{ $user == old('user_id', $order->user_id) ? ' selected' : '' }}>{{ $label }}</option>
+        @endforeach;
+    </select>
+    @if ($errors->has('user_id'))
+        <span class="invalid-feedback"><strong>{{ $errors->first('user_id') }}</strong></span>
+    @endif
+</div>
+
+<div class="form-group">
+    <label for="product_id" class="col-form-label">Product</label>
+    <select id="product_id" class="form-control{{ $errors->has('product_id') ? ' is-invalid' : '' }}" name="product_id">
+        @foreach ($products as $product => $label)
+            <option value="{{ $product }}"{{ $product == old('product_id',$order->product_id) ? ' selected' : '' }}>{{ $label }}</option>
+        @endforeach;
+    </select>
+    @if ($errors->has('product_id'))
+        <span class="invalid-feedback"><strong>{{ $errors->first('product_id') }}</strong></span>
+    @endif
+</div>
+
+<div class="form-group">
+    <label for="quantity" class="col-form-label">Quantity</label>
+    <input type="number" class="form-control{{ $errors->has('quantity') ? ' is-invalid' : '' }}" name="quantity" value="{{ old('quantity', $order->quantity) }}">
+    @if ($errors->has('quantity'))
+        <span class="invalid-feedback"><strong>{{ $errors->first('quantity') }}</strong></span>
+    @endif
+</div>
+
+<div class="form-group">
+    <button type="submit" class="btn btn-primary">{{ $button }}</button>
+</div>
